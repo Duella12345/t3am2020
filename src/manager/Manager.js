88e875f8605ebb3867beb4a6627493f3d99c587f
Duella12345 <33983143+Duella12345@users.noreply.github.com>
2020-07-23 14:50:38 +0100

    add bootstrap and template managers page

--- src/manager/Manager.js
+++ src/manager/Manager.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Manager = () => {
     return (
         <div>
-            Manager Screen
+           <h1> Manager Screen </h1>
+
+            <h3>To search for a visitor please input their id: </h3>
+
+            output list of in and out times and duration 
+
+            <h3>To see all visitor logs click here: </h3>
         </div>
     );
 };
